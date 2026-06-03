88a5f05fb5da75748fa492ecfcc97cc3c489753e
undesputed <carrieyu67@gmail.com>
2026-06-03 15:28:06 +0800

    feat: gesture recognizer model URL (replaces hand landmarker)

--- frontend/config.js
+++ frontend/config.js
@@ -3,7 +3,7 @@ export const CONFIG = {
   WASM_BASE: "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.35/wasm",
   MODEL_URL: "https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task",
   POSE_MODEL_URL: "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task",
-  HAND_MODEL_URL: "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task",
+  GESTURE_MODEL_URL: "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
   POSE_THROTTLE_MS: 120,  // run pose + hands ~8x/sec
   // Blendshapes forwarded to the backend (smile, blink, brow, and 8 eyeLook* for gaze)
   BLENDSHAPES: ["mouthSmileLeft", "mouthSmileRight", "eyeBlinkLeft", "eyeBlinkRight", "browInnerUp",
