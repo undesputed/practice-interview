35d805ed56e605a60f1335c76d1cbadc7a58bdfc
undesputed <carrieyu67@gmail.com>
2026-06-03 17:41:17 +0800

    feat: object-detector model URL + tension blendshapes

--- frontend/config.js
+++ frontend/config.js
@@ -4,11 +4,15 @@ export const CONFIG = {
   MODEL_URL: "https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task",
   POSE_MODEL_URL: "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task",
   GESTURE_MODEL_URL: "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
+  OBJECT_MODEL_URL: "https://storage.googleapis.com/mediapipe-models/object_detector/efficientdet_lite0/float16/1/efficientdet_lite0.tflite",
   POSE_THROTTLE_MS: 120,  // run pose + hands ~8x/sec
   // Blendshapes forwarded to the backend (smile, blink, brow, and 8 eyeLook* for gaze)
   BLENDSHAPES: ["mouthSmileLeft", "mouthSmileRight", "eyeBlinkLeft", "eyeBlinkRight", "browInnerUp",
     "eyeLookInLeft", "eyeLookInRight", "eyeLookOutLeft", "eyeLookOutRight",
     "eyeLookUpLeft", "eyeLookUpRight", "eyeLookDownLeft", "eyeLookDownRight",
-    "jawOpen", "browOuterUpLeft", "browOuterUpRight"],
+    "jawOpen", "browOuterUpLeft", "browOuterUpRight",
+    "eyeSquintLeft", "eyeSquintRight", "mouthPressLeft", "mouthPressRight",
+    "browDownLeft", "browDownRight", "jawLeft", "jawRight",
+    "noseSneerLeft", "noseSneerRight", "mouthFrownLeft", "mouthFrownRight"],
   ROLES: ["Software Engineer", "Product Manager", "Data Analyst", "Customer Support"],
 };
